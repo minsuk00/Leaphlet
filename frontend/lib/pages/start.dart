dce22871ce1ff02c40922d2f35f986bced08d347
Minsuk Choi <51175254+kusnim1121@users.noreply.github.com>
2023-12-27 20:51:52 +0900

    implement start page

---
+++ frontend/lib/pages/start.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:test/pages/exhibitor/home_exhibitor.dart';
+import 'package:test/pages/organizer/home_organizer.dart';
+import 'package:test/pages/visitor/home_visitor.dart';
+
+class StartPage extends StatelessWidget {
+  // const StartPage({super.key});
+  const StartPage(this.username, {super.key});
+
+  final String username;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              onPressed: () => moveToPage(context, const VisitorHomePage()),
+              child: const Text("Use as Visitor"),
+            ),
+            OutlinedButton(
+              onPressed: () => moveToPage(context, const ExhibitorHomePage()),
+              child: const Text("Use as Exhibitor"),
+            ),
+            OutlinedButton(
+              onPressed: () => moveToPage(context, const OrganizerHomePage()),
+              child: const Text("Use as Organizer"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void moveToPage(BuildContext context, StatefulWidget targetPage) {
+  Navigator.push(context, MaterialPageRoute(builder: (_) => targetPage));
+}
