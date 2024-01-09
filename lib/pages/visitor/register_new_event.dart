fb306dafd620eb1e0bc556c928beb8d2119c656c
Minsuk Choi <51175254+kusnim1121@users.noreply.github.com>
2024-01-10 04:12:23 +0900

    misc

--- lib/pages/visitor/register_new_event.dart
+++ lib/pages/visitor/register_new_event.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test/pages/util/navigate.dart';
+import 'package:test/util/navigate.dart';
 
 class RegisterNewEventPage extends StatefulWidget {
   const RegisterNewEventPage({super.key});
