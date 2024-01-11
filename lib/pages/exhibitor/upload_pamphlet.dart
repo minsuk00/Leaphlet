33f6fae2d1bf6ba6c40fcd4413276474119c14e5
Ryudai Inoue <inoueryudai@gmail.com>
2024-01-11 14:35:02 +0900

    fixed the padding overflow error

--- lib/pages/exhibitor/upload_pamphlet.dart
+++ lib/pages/exhibitor/upload_pamphlet.dart
@@ -30,141 +30,149 @@ class _UploadPamphletPageState extends State<UploadPamphletPage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double paddingValue = screenWidth * 0.15;
     return Scaffold(
-        appBar: AppBar(
-          leading: const BackButton(),
-          title: const Text("Upload Pamphlet"),
-        ),
-        body: Container(
-            padding: const EdgeInsets.all(150),
-            child: Form(
-                key: _formKey, // Set the key to the form
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      TextFormField(
-                        controller: eventCodeInput,
-                        autofocus: false,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Event Name',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the event code';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: boothNumberInput,
-                        autofocus: false,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Booth Number',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the booth number';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: orgNameInput,
-                        autofocus: false,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Organization Name',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the organization name';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: yourNameInput,
-                        autofocus: false,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Your Name',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your name';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: emailAddressInput,
-                        autofocus: false,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Email Address',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the email address';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextField(
-                        controller: phoneNumberInput,
-                        autofocus: false,
-                        textInputAction: TextInputAction.next,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Phone Number',
-                        ),
-                      ),
-                      TextFormField(
-                        readOnly: true,
-                        controller: pamphletInput,
-                        onTap: () async {
-                          FilePickerResult? result =
-                              await FilePicker.platform.pickFiles(
-                            type: FileType.custom,
-                            allowedExtensions: ['pdf'],
-                          );
-                          if (result != null) {
-                            String filePath = result.files.single.path!;
-                            // Do something with the file path (store it or display the file name, etc.)
-                            pamphletInput.text =
-                                filePath.substring(filePath.lastIndexOf("/")+1);
-                            if (kDebugMode) {
-                              print('Selected file: $filePath');
-                            }
-                          }
-                        },
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Upload Pamphlet',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please upload the pamphlet';
-                          }
-                          return null;
-                        },
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            moveToPage(context, const ConfirmationPage());
-                          }
-                        },
-                        child: const Text('Confirm'),
-                      ),
-                    ]))));
+      appBar: AppBar(
+        leading: const BackButton(),
+        title: const Text("Upload Pamphlet"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(paddingValue),
+          child: Form(
+            key: _formKey, // Set the key to the form
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: eventCodeInput,
+                  autofocus: false,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Event Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the event code';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: boothNumberInput,
+                  autofocus: false,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Booth Number',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the booth number';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: orgNameInput,
+                  autofocus: false,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Organization Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the organization name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: yourNameInput,
+                  autofocus: false,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Your Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: emailAddressInput,
+                  autofocus: false,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email Address',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the email address';
+                    }
+                    return null;
+                  },
+                ),
+                TextField(
+                  controller: phoneNumberInput,
+                  autofocus: false,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone Number',
+                  ),
+                ),
+                TextFormField(
+                  readOnly: true,
+                  controller: pamphletInput,
+                  onTap: () async {
+                    FilePickerResult? result =
+                        await FilePicker.platform.pickFiles(
+                      type: FileType.custom,
+                      allowedExtensions: ['pdf'],
+                    );
+                    if (result != null) {
+                      String filePath = result.files.single.path!;
+                      // Do something with the file path (store it or display the file name, etc.)
+                      pamphletInput.text =
+                          filePath.substring(filePath.lastIndexOf("/")+1);
+                      if (kDebugMode) {
+                        print('Selected file: $filePath');
+                      }
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Upload Pamphlet',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please upload the pamphlet';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      moveToPage(context, const ConfirmationPage());
+                    }
+                  },
+                  child: const Text('Confirm'),
+                ),
+              ]
+            )
+          )
+        )
+      )
+    );
   }
 }
