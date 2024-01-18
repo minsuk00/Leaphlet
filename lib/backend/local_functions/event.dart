236bc224cc396e2954239dae59ebf0810f77e2e4
Minsuk Choi <51175254+kusnim1121@users.noreply.github.com>
2024-01-19 05:06:43 +0900

    implement backend code for local file io of event info

---
+++ lib/backend/local_functions/event.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:test/backend/local_functions/util.dart';
+import 'package:test/util/user_type.dart';
+
+// TODO: Check if already registered?
+Future<void> saveEventToLocalFile(
+    Map<String, dynamic> newEventObject, UserType userType) async {
+  //assign file according to user type
+  String userTypeString = userType.name;
+  final fileName = "event_code_$userTypeString";
+
+  //get file content
+  final List<dynamic> eventList = await getLocalFileContent(fileName);
+
+  //append new item to json list
+  eventList.add(newEventObject);
+
+  print("########### WRITING TO FILE $eventList");
+  //convert json to String and update file
+  writeToLocalFile(fileName, jsonEncode(eventList));
+}
+
+Future<List<dynamic>> getEventListFromLocalFile(UserType userType) async {
+  //assign file according to user type
+  String userTypeString = userType.name;
+  final fileName = "event_code_$userTypeString";
+  return getLocalFileContent(fileName);
+}
+
+void resetLocalEventList(UserType userType) {
+  String userTypeString = userType.name;
+  final fileName = "event_code_$userTypeString";
+  resetFile(fileName);
+}
