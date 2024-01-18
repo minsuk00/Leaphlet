b2441dd90a7d340132eb02a1e84b93e7695d8d69
Minsuk Choi <51175254+kusnim1121@users.noreply.github.com>
2024-01-19 05:53:26 +0900

    misc

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 // import 'dart:io';
 
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:test/pages/login.dart';
 
 import 'firebase_options.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
@@ -19,17 +19,17 @@ Future<void> main() async {
   );
 
 
-  if (kDebugMode) {
-   try {
-    //  debugPrint('##### using emulator #####');
-    //  debugPrint(Platform.isAndroid.toString());
-     FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
-     await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-   } catch (e) {
-     // ignore: avoid_print
-     print(e);
-   }
- }
+  // if (kDebugMode) {
+  //  try {
+  //   //  debugPrint('##### using emulator #####');
+  //   //  debugPrint(Platform.isAndroid.toString());
+  //    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  //    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  //  } catch (e) {
+  //    // ignore: avoid_print
+  //    print(e);
+  //  }
+//  }
   runApp(const MyApp());
 }
 
